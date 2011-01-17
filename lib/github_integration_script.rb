ea5e78bac0b6e0bfd3c2b4a43f80058c37aa64b9
Ryan Weald <ryan@weald.com>
2011-01-16 21:10:58 -0800

    refactor into a class within the module

--- lib/github_integration_script.rb
+++ lib/github_integration_script.rb
@@ -3,49 +3,59 @@ require "rubygems"
 require "rest-client"
 require "redis"
 require "json"
-
-# class ClassName
-#   attr_accessor :user
-#   attr_accessor :repository
-#   attr_reader :redis
-# 
-#   def initialize(args)
-#     @user = args[:user]
-#     @repository = args[:repository]
-#     @redis = Redis.new
-#   end
-#   
-#   def get_pull_requests
-#     requests = RestClient.get "#{GITHUB}/#{@user}/#{@repository}"
-#     self.store_unseen_requests requests
-#   end
-#   
-#   private
-#   def store_unseen_requests(requests)
-#     requests.each do |req|
-#       if !(@redis.sadd "pull_requests", req)
-#         @redis.lpush "unprocessed", req
-#       end
-#     end
-#   end
-# end
+require "fileutils"
 
 
 module GithubIntegration
   GITHUB = "http://github.com/api/v2/json/pulls"
   FILEPATH = File.expand_path("~/Computer Programs/cogs120/snack-picker-test")
   
-  def self.init
-    @user = "rweald"
-    @repository = "snack-picker"
-  end
-  
-  def self.get_pull_requests
-    self.init
-    route = "#{GITHUB}/#{@user}/#{@repository}/closed"
-    response = RestClient.get route
-    pull_requests = JSON.parse(response)
-    return pull_requests["pulls"]
-    # self.store_unseen_requests requests
+  class APIRequest
+    attr_accessor :redis
+    attr_accessor :user
+    attr_accessor :repository
+    def initialize(args)
+      @user = args[:user]
+      @repository = args[:repository]
+      @redis = Redis.new
+    end
+    
+    def get_pull_requests
+      route = "#{GITHUB}/#{@user}/#{@repository}"
+      response = RestClient.get route
+      pull_requests = JSON.parse(response)
+      return pull_requests["pulls"]
+      # self.store_unseen_requests requests
+    end
+    
+    def store_new_pull_requests(pull_requests)
+      pull_requests.each do |req|
+        if (@redis.sadd "pull_requests", req)
+          @redis.lpush "test_queue", req
+        end
+      end
+    end
+    
+    def run_test_suite
+      self.init
+      FileUtils.cd(FILEPATH)
+      result = `rspec --format d spec/`
+      saved_result = []
+      result = result.split("seconds")[1]
+      result = result.split(",")
+      result.each do |entry|
+        saved_result << entry.strip.split(" ")[0]
+      end
+      @redis.lpush "results" , "#{result = result.split(",")}"
+    end
+    
+    
+    def checkout_pull_request(args={})
+      FileUtils.cd(FILEPATH)
+      cmd_string = "git checkout -b #{args[:branch_name]}:#{args[:user_name]} master"
+      system(cmd_string)
+      cmd_string = "git pull #{args[:url]} #{args[:branch_name]}"
+      system(cmd_string)
+    end
   end
 end
