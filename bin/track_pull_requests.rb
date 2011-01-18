21b4be0684f195c7e5915f17e8f11e1f06344ee5
Ryan Weald <ryan@weald.com>
2011-01-17 18:44:47 -0800

    created the plist that can be used for launchd

--- bin/track_pull_requests.rb
+++ bin/track_pull_requests.rb
@@ -1,10 +1,9 @@
+#!/usr/bin/env ruby
 require "rubygems"
-
 require "redis"
 require "JSON"
-require "ruby-debug"
-$LOAD_PATH.push("/Users/Ryan/Computer Programs/cogs120/pull-tracker/lib")
 require "github_integration_script.rb"
+$LOAD_PATH.push("/Users/Ryan/Computer Programs/cogs120/pull-tracker/lib")
 
 include GithubIntegration
 
@@ -17,7 +16,7 @@ while (g.redis.llen("test_queue") > 0)
   req = JSON.parse(g.redis.lpop("test_queue"))
   req = req["head"]
   g.checkout_pull_request :branch_name => req["ref"], :user_name => req["repository"]["owner"], :url => req["repository"]["url"]
-  
   g.run_test_suite req["label"]
 end
 
+puts "This is proof that I have been run"
