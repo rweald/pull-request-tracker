74966c48dccd9ca4884c159b2d754e66bb9439d6
Ryan Weald <ryan@weald.com>
2011-01-17 16:29:06 -0800

    completed basic implementation. created shell script that can be run to do all the necessary steps
    Signed-off-by: Ryan Weald <ryan@weald.com>

---
+++ shell script.rb
@@ -0,0 +1,24 @@
+require "rubygems"
+
+require "redis"
+require "JSON"
+require "ruby-debug"
+$LOAD_PATH.push("/Users/Ryan/Computer Programs/cogs120/pull-tracker/lib")
+require "github_integration_script.rb"
+
+include GithubIntegration
+
+g = GithubIntegration::APIRequest.new :user => "icl", :repository => "cove"
+
+# get the requests 
+g.store_new_pull_requests(g.get_pull_requests)
+debugger
+
+while (g.redis.llen("test_queue") > 0)
+  req = JSON.parse(g.redis.lpop("test_queue"))
+  req = req["head"]
+  g.checkout_pull_request :branch_name => req["ref"], :user_name => req["repository"]["owner"], :url => req["repository"]["url"]
+  
+  g.run_test_suite req["label"]
+end
+
